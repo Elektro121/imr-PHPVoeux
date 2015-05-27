2096077b20460e03d609cfa6c575eb20e233ba55
Elektro Korobase <korobase.elektro@gmail.com>
2015-05-27 09:46:43 +0200

    Installation Bootstrap

---
+++ application/views/bootstrap.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>">
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
