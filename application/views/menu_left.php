b92ba097e6ae1adbe1c5231e1a22517934e1af08
viincerome <viincerome@gmail.com>
2015-06-04 16:02:57 +0200

    Ajout de menu_left.php

--- application/views/menu_left.php
+++ application/views/menu_left.php
@@ -1,17 +1,11 @@
-<div id="menu-left">
-        <ul>
+<div class="col-sm-3 col-md-2">
 
-            <li><a href=""></a></li>
+    <ul class="nav nav-sidebar">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Heures par enseignant</a></li>
+        <li><a href="#">Heures par modules</a></li>
+        <li><a href="#">Reservation des heures</a></li>
+        <li><a href="#">Deconnexion</a></li>
+    </ul>
 
-            <li><a href="">Accueil</a></li>
-
-            <li><a href="">Heures par enseignant</a></li>
-
-            <li><a href="">Heures par modules</a></li>
-
-            <li><a href="">Reservation des heures</a></li>
-
-            <li><a href="">Deconnexion</a></li>
-
-        </ul>
 </div>
