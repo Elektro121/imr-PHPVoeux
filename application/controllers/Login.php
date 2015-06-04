bcbb17b2d30d8508cfeaa88ff04e020f63e13c6e
Elektro Korobase <korobase.elektro@gmail.com>
2015-06-04 16:29:37 +0200

    Second draft login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,11 +5,40 @@
  * Time: 17:31
  */
 
+/**
+ * Class m_user
+ * @property m_user $m_user Optional
+ */
 class Login extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model("m_user");
+    }
 
     public function index()
     {
         $this->load->helper("url");
-        $this->load->view("login");
+        $this->load->helper("form");
+        //$this->load->library('session');
+        $this->load->library("form_validation");
+
+        $data['title'] = "Se connecter";
+        $this->load->view("header", $data);
+        $this->form_validation->set_rules('user', 'Nom d\'utilisateur', 'required');
+        $this->form_validation->set_rules('password', 'Mot de passe', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view("login");
+        }
+        else {
+            if($this->m_user->check_password($this->input->post("user"),$this->input->post("password")) == TRUE) {
+                $data['user'] = $this->input->post("user");
+                $this->load->view("login", $data);
+            } else {
+                $data['login_error'] = "Oh ! Il existe une erreur dans la combinaison Utilisateur / Mot de passe, veuillez réessayer.";
+                $this->load->view("login", $data);
+            }
+        }
     }
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,12 +1,25 @@
 <div class="container">
 
-    <?php ?>
+    <?php if(!isset($user)):?>
+
+        <?php echo form_open("Login") ?>
         <h2 class="form-signin-heading">Connectez vous</h2>
-        <label for="inputEmail" class="sr-only">Nom d'utilisateur</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Nom d'utilisateur" required autofocus>
-        <label for="inputPassword" class="sr-only">Mot de passe</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Mot de Passe" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
-    </form>
+            <?php if(validation_errors() != null):?>
+                <div class="alert alert-danger" role="alert"><?php echo validation_errors(); ?></div>
+            <?php endif;?>
+            <?php if(isset($login_error)):?>
+                <div class="alert alert-danger" role="alert"><?=$login_error?></div>
+            <?php endif;?>
+            <label for="user" class="sr-only">Nom d'utilisateur</label>
+            <input type="text" name="user" class="form-control" placeholder="Nom d'utilisateur" required autofocus>
+            <label for="password" class="sr-only">Mot de passe</label>
+            <input type="password" name="password" class="form-control" placeholder="Mot de passe" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Se connecter</button>
+        </form>
+    <?php endif;?>
+
+    <?php if(isset($user)):?>
+        <h2>Bonjour <?=$user?> ! Vous êtes déjà connecté.</h2>
+    <?php endif;?>
 
 </div>
