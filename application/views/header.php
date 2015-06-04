beb956528b7c473571e92a1433ee8abf47e0418f
viincerome <viincerome@gmail.com>
2015-06-04 16:34:44 +0200

    Ajouter header.php

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<html>
+
+<head lang="en">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?php echo base_url('application/assets/css/bootstrap.min.css'); ?>">
+    <script type="text/javascript" src="<?php echo base_url('application/assets/js/bootstrap.min.js'); ?>">
+    </script>
+    <title><?php echo '$title;' ?> - Gestionnaire d'heure de cours</title>
+</head>
+
+<body>
