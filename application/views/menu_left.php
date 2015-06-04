7c5c58f39a942226b928ca981ea539de2b289c40
viincerome <viincerome@gmail.com>
2015-06-04 16:04:40 +0200

    changement dans menu_left.php

--- application/views/menu_left.php
+++ application/views/menu_left.php
@@ -1,11 +1,11 @@
 <div class="col-sm-3 col-md-2">
 
     <ul class="nav nav-sidebar">
-        <li><a href="#">Accueil</a></li>
-        <li><a href="#">Heures par enseignant</a></li>
-        <li><a href="#">Heures par modules</a></li>
-        <li><a href="#">Reservation des heures</a></li>
-        <li><a href="#">Deconnexion</a></li>
+        <li><a>Accueil</a></li>
+        <li><a>Heures par enseignant</a></li>
+        <li><a>Heures par modules</a></li>
+        <li><a>Reservation des heures</a></li>
+        <li><a>Deconnexion</a></li>
     </ul>
 
 </div>
