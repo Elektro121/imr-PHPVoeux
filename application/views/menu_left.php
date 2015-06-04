6095c4ee62281dc22d7685caf98ff8ad3217461d
viincerome <viincerome@gmail.com>
2015-06-04 15:50:16 +0200

    Ajout menu_left.php

---
+++ application/views/menu_left.php
@@ -0,0 +1,17 @@
+<div id="menu-left">
+        <ul>
+
+            <li><a href=""></a></li>
+
+            <li><a href="">Accueil</a></li>
+
+            <li><a href="">Heures par enseignant</a></li>
+
+            <li><a href="">Heures par modules</a></li>
+
+            <li><a href="">Reservation des heures</a></li>
+
+            <li><a href="">Deconnexion</a></li>
+
+        </ul>
+</div>
