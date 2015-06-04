fd9fbd570b06989569eb9023d3a3ecde5e21dd27
viincerome <viincerome@gmail.com>
2015-06-04 15:15:24 +0200

    Ajout header.php

---
+++ application/views/Header.php
@@ -0,0 +1,11 @@
+<html>
+
+    <head lang="en">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
+        <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>">
+        </script>
+        <title>Projet PhP</title>
+    </head>
+
+<body>
