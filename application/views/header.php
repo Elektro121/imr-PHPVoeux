be833cb0addaa7fdc62dc8d81d0f748eaa1a2d0d
Elektro Korobase <korobase.elektro@gmail.com>
2015-06-04 17:00:29 +0200

    add jquery

--- application/views/header.php
+++ application/views/header.php
@@ -2,10 +2,10 @@
 
 <head lang="en">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="<?php echo base_url('application/assets/css/bootstrap.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('application/assets/css/main.css'); ?>">
-    <script type="text/javascript" src="<?php echo base_url('application/assets/js/bootstrap.min.js'); ?>">
-    </script>
+    <link rel="stylesheet" href="<?php echo base_url('application/assets/css/bootstrap.min.css'); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url('application/assets/css/main.css'); ?>" />
+    <script type="text/javascript" src="<?php echo base_url('application/assets/js/bootstrap.min.js'); ?>" />
+    <script type="text/javascript" src="<?php echo base_url('application/assets/js/jquery.min.js'); ?>" />
     <title><?php echo '$title;' ?> - Gestionnaire d'heure de cours</title>
 </head>
 
