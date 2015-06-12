b81df8a0d6df7ee13d1b178dbe47004ae2b8236d
viincerome <viincerome@gmail.com>
2015-06-12 09:00:48 +0200

    Modif Finale Head.php

--- application/views/head.php
+++ application/views/head.php
@@ -2,10 +2,10 @@
     <div class="container-fluid">
         <div class="navbar-header:before">
             <a>
-                <img alt="Brand" src="<?php echo base_url('application/assets/Images/Enssat_logo.png'); ?>" alt="Logo école" style="float:left">
+                <img alt="Brand" src="<?php echo base_url('application/assets/Images/Enssat_logo.png'); ?>" alt="Logo école" style="float:left" width="100" height="">
             </a>
             <a>
-                <img alt="Brand" src="<?php echo base_url('application/assets/Images/Universite_logo.png'); ?>" alt="Logo Université" style="float:right">
+                <img alt="Brand" src="<?php echo base_url('application/assets/Images/Universite_logo.png'); ?>" alt="Logo Université" style="float:right" width="100" height="40">
             </a>
         </div>
     </div>
