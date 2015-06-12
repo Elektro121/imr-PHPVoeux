6ed36132aeeb9e62d27a561142de854751bff96b
viincerome <viincerome@gmail.com>
2015-06-12 09:25:24 +0200

    Suppression menu_left_admin
    Mise en plus de tout le menu dans la meme page

--- application/views/menu_left.php
+++ application/views/menu_left.php
@@ -1,12 +1,27 @@
-<div class="col-sm-3 col-md-2">
+<div class="col-sm-3 col-md-3">
 
     <ul class="nav nav-sidebar">
-        <li><h1 href="">Panneau d'administration</h1></li>
-        <li><a>Accueil</a></li>
-        <li><a>Heures par enseignant</a></li>
-        <li><a>Heures par modules</a></li>
-        <li><a>Reservation des heures</a></li>
-        <li><a>Deconnexion</a></li>
+        <li><h4 href=""><font color="#337ab7"><b>Panneau d'administration</b></font></h4></li>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Mes heures</a></li>
+        <li><a href="">Modules</a></li>
+        <li><a href="">Réservation des heures</a></li>
+        <li><a>Panneau de configuration</a></li>
+        <ul>
+            <li><a href="">Vu de mes heures</a></li>
+            <li><a href="">Vu de tous</a></li>
+        </ul>
+        <li><a></a></li>
+    </ul>
+
+    <ul class="nav nav-sidebar">
+        <li><h4 href=""><font color="#337ab7"><b>Partie Administrateur</b></font></h4></li>
+        <li><a href="">Gestion des utilisateurs</a></li>
+        <li><a href="">Gestion des HETD</a></li>
+        <li><a href="">Gestion des modules</a></li>
+        <li><a href="">Gestion des décharges</a></li>
+
+        <li><a href="">Déconnexion</a></li>
     </ul>
 
 </div>
