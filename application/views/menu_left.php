213393e46b6c25628fe1520ae9deb47e8f135b22
viincerome <viincerome@gmail.com>
2015-06-15 17:17:04 +0200

    Mise en place de l'onglet Récapitulatif des modules

--- application/views/menu_left.php
+++ application/views/menu_left.php
@@ -4,8 +4,8 @@
         <li><h4><font color="#337ab7"><b>Panneau d'administration</b></font></h4></li>
         <li><?php echo anchor("Dashboard", "Accueil")?></li>
         <li><?php echo anchor("Assignements", "Mes heures")?></li>
-        <li><?php echo anchor("View/Heure_module", "Récapitulatif des Modules")?></li>
         <li><?php echo anchor("Assignements/Inscription", "Réservation des Heures")?></li>
+        <li><?php echo anchor("View/Heure_module", "Récapitulatif des Modules")?></li>
         <li><?php echo anchor("", "Non actif / Heures par Modules")?></li>
         <li><?php echo anchor("", " Non actif / Heures par Enseignants")?></li>
         <li><?php echo anchor("Login/Disconnect", "Déconnexion")?></li>
